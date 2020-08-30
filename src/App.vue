e57cb86ede7e102170fc00bff457acd0430e6b15
19971202 <t.takahashi19971202@gmail.com>
2020-08-30 14:04:01 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <Address />
+  </div>
+</template>
+
+<script>
+import Address from "./components/Address";
+export default {
+  components: {
+    Address
+  }
+};
+</script>
---
+++ src/components/Address.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="address">
+    <input type="text" v-model="text" />
+    <button @click="add">住所自動入力</button>
+    <p>Address:{{ id }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      text:"",
+      id:""
+    };
+  },
+  methods: {
+      async add() {
+      let item =  await axios.get(
+      `https://apis.postcode-jp.com/api/v4/postcodes/${this.text}?apiKey=Bjyqpn5O3i7Cfybt80iSLbO9JLTYYfnA4Y12apL`
+       );
+      this.id = item.data[0].allAddress;
+      console.log(item);
+    }
+  },
+};
+
+
+</script>
